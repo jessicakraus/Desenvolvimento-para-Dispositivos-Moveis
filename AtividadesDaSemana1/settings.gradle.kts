b3477d4081355cf3a7ec2d4f2acb4785517dc219
Jéssica Kraus <jessica_kraus@outlook.com>
2023-09-11 18:29:21 -0300

    Exercício 1 completo

---
+++ AtividadesDaSemana1/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Atividades da Semana 1"
+include(":app")
